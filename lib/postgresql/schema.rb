0a86a127ea3942f254bcabc2f8aa7baec11b63cd
Kip Cole <kipcole9@gmail.com>
2014-10-25 21:57:21 +0900

    Refactoring and gem structuring for rails apps

--- lib/postgresql/schema.rb
+++ lib/postgresql/schema.rb
@@ -1,5 +1,9 @@
 require "postgresql/schema/version"
 
+dir = File.dirname(__FILE__)
+require "#{dir}/schema/schema_statements.rb"
+require "#{dir}/schema/schema_dumper.rb"
+
 module Postgresql
   module Schema
     # Your code goes here...
--- lib/postgresql/schema/railtie.rb
+++ lib/postgresql/schema/railtie.rb
@@ -2,9 +2,7 @@ module Postgresql
   module Inheritance
     class Railtie < Rails::Railtie
       initializer "postgres-inheritance.configure_rails_initialization" do
-        dir = File.dirname(__FILE__)
-        load "#{dir}/schema_statements.rb"
-        load "#{dir}/schema_dumper.rb"
+
       end
     end
   end
